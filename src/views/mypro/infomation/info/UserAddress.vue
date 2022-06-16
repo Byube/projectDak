e98e3315c454f1e73f33e77c576595590d71f6a3
jeffkim <jeffkim@korfinholdings.com>
2022-06-16 13:47:35 +0900

    #0012 주소input 수정

--- src/views/mypro/infomation/info/UserAddress.vue
+++ src/views/mypro/infomation/info/UserAddress.vue
@@ -1,28 +1,53 @@
 <template>
   <div class="card">
-      <div class="bg-blue-300 h-22rem w-4 ">
-
-      </div>
-    <div class="flex align-items-center justify-content-start">
-      <div class="font-medium text-black-alpha-60 border-round p-inputgroup">
-        <InputText placeholder="우편번호" :modelValue="postal" />
-        <Button
-          label="주소검색"
-          class="bg-indigolight p-inputgroup-addon"
-          @click="searchAddress"
-        />
+    <div class="flex mt-5 align-items-center justify-content-center">
+      <div class="h-16rem w-3">
+        <!-- 우편번호 -->
+        <div class="flex align-items-center justify-content-start p-inputgroup">
+          <InputText
+            placeholder="우편번호"
+            :modelValue="postal"
+            :readonly="true"
+          />
+          <Button
+            label="주소검색"
+            class="bg-indigolight p-inputgroup-addon"
+            @click="searchAddress"
+          />
+        </div>
+        <!-- 우편번호 -->
+        <!-- 주소 -->
+        <div class="flex mt-3 align-items-center justify-content-start">
+          <InputText
+            placeholder="주소"
+            :modelValue="address"
+            class="w-full"
+            :readonly="true"
+          />
+        </div>
+        <!-- 주소 -->
+        <!-- 상세주소 -->
+        <div class="flex mt-3 align-items-center justify-content-start">
+          <InputText
+            placeholder="상세주소"
+            :modelValue="detailAddress"
+            class="w-full"
+          />
+        </div>
+        <!-- 상세주소 -->
       </div>
     </div>
-    <div class="flex mt-3 align-items-center justify-content-start">
-      <div class="font-medium text-black-alpha-60 border-round">
-        <InputText placeholder="주소" :modelValue="address" />
-      </div>
+    <!-- foot -->
+    <div class="flex mt-3 align-items-center justify-content-between">
+      <Button label="Back" @click="prevPage()" icon="pi pi-angle-left"></Button>
+      <Button
+        label="Next"
+        @click="nextPage()"
+        icon="pi pi-angle-right"
+        icon-pos="right"
+      ></Button>
     </div>
-    <div class="flex mt-3 align-items-center justify-content-start">
-      <div class="font-medium text-black-alpha-60 border-round">
-        <InputText placeholder="상세주소" :modelValue="detailAddress" />
-      </div>
-    </div>
+    <!-- foot -->
     <!-- 카카오주소 -->
     <div class="post-daum-address">
       <Dialog
@@ -38,17 +63,6 @@
       </Dialog>
     </div>
     <!-- 카카오주소 -->
-    <!-- foot -->
-    <div class="flex mt-5 align-items-center justify-content-between">
-      <Button label="Back" @click="prevPage()" icon="pi pi-angle-left"></Button>
-      <Button
-        label="Next"
-        @click="nextPage()"
-        icon="pi pi-angle-right"
-        icon-pos="right"
-      ></Button>
-    </div>
-    <!-- foot -->
   </div>
 </template>
 
